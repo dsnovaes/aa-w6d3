c51eed3ba6385631f169c31d3d84cee47b9c613b
Diego Novaes <dsnovaes1@gmail.com>
2022-11-09 22:25:25 -0800

    phase 4 completed

--- first_routes_n_controllers/app/controllers/users_controller.rb
+++ first_routes_n_controllers/app/controllers/users_controller.rb
@@ -1,19 +1,42 @@
 class UsersController < ApplicationController
 
   def index
-    render json: params
-    # render plain: "I'm in the index action!"
+    render json: User.all
   end
 
   def show
-    render json: params
+    render json: User.find_by(id: params[:id])
   end
 
   def create
-    user = User.new(params.require(:user).permit(:name, :email))
-    # replace the `user_attributes_here` with the actual attribute keys
-    user.save!
+    user = User.new(user_params)
+    user.save
     render json: user
   end
+
+  def update
+    user = User.find_by(id: params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    if user.nil?
+      raise "user not found"
+    else
+      user.destroy
+      redirect_to users_url
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 
 end
