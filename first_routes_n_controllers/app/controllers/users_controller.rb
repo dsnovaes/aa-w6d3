98fe8f1019fb3d90963e6effccaf788e691f92f0
Diego Novaes <dsnovaes1@gmail.com>
2022-11-09 16:50:49 -0800

    phase 3 completed

--- first_routes_n_controllers/app/controllers/users_controller.rb
+++ first_routes_n_controllers/app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
 
   def index
+    render json: params
+    # render plain: "I'm in the index action!"
+  end
 
+  def show
+    render json: params
+  end
+
+  def create
+    render json: params
   end
 
 end
